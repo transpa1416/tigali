52ad5c79561996add803d3d49102abc683aa70fc
avillarreal <avillarreal@casasaguilar.com.mx>
2021-11-16 09:46:45 -0600

    Corrección login

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -26,7 +26,7 @@ class CustomAuthController extends Controller
 
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return redirect()->intended('dashboard')
+            return redirect()->intended('course/writer')
                         ->withSuccess('Signed in');
         }
 
