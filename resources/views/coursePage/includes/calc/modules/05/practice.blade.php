42e95ce59e8764f56812e0d8c58ad765029eb24c
avillarreal <avillarreal@casasaguilar.com.mx>
2021-11-25 09:29:39 -0600

    corección video

--- resources/views/coursePage/includes/calc/modules/05/practice.blade.php
+++ resources/views/coursePage/includes/calc/modules/05/practice.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/EqQtXECSLD4?rel=0" frameborder="0" allowfullscreen></iframe>
+        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/oJSJAToHa-Y?rel=0" frameborder="0" allowfullscreen></iframe>
     </div>
 @endsection
